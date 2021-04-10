7346115650ccb50a5ac385948d8eb376a8acbe98
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-09 23:26:32 -0500

    Fixing comment scripts for travis

--- scripts/travis_post_failure.sh
+++ scripts/travis_post_failure.sh
@@ -1,4 +1,4 @@
-if ["$TRAVIS_PULL_REQUEST" != "false"] ; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"FAILURE check Travis CI logs and revise\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
--- scripts/travis_post_success.sh
+++ scripts/travis_post_success.sh
@@ -1,4 +1,4 @@
-if ["$TRAVIS_PULL_REQUEST" != "false"] ; then
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"SUCCESS deployed version to staging website.\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
