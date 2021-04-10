bf43095816b086a565c73250893fa50833f6e4fc
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-10 00:00:07 -0500

    Adding back matrix

--- scripts/travis_post_failure.sh
+++ scripts/travis_post_failure.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    curl -u toastboard${GITHUB_TOKEN} \
     -d "{\"body\": \"FAILURE check Travis CI logs and revise\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
