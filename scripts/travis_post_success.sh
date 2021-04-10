211fcc1db548e56ab63ed95f1ce2eefe20d82df5
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-09 23:48:28 -0500

    Again trying to fix .sh

--- scripts/travis_post_success.sh
+++ scripts/travis_post_success.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
+    curl -u token toastboard:${GITHUB_TOKEN} \
     -d "{\"body\": \"SUCCESS deployed version to staging website shown in deployment message\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
