31766b303535afbadae18c4432c62a0c7a274752
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-09 23:37:16 -0500

    Edit .sh file

--- scripts/travis_post_success.sh
+++ scripts/travis_post_success.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"SUCCESS deployed version to staging website.\"}" \
+    curl -H "Authorization: token ${GITHUB_TOKEN}:x-oauth-basic" -X POST \
+    -d "{\"body\": \"SUCCESS deployed version to staging website shown in deployment message\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
