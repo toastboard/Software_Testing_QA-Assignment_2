50e0f2517e815faa83198a274b0cde5c3cb7f6bd
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-09 23:51:34 -0500

    Trying to change .sh again

--- scripts/travis_post_success.sh
+++ scripts/travis_post_success.sh
@@ -1,5 +1,5 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    curl -u token toastboard:${GITHUB_TOKEN} \
+    curl -H "Authorization: token ${GITHUB_TOKEN}"\
     -d "{\"body\": \"SUCCESS deployed version to staging website shown in deployment message\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
