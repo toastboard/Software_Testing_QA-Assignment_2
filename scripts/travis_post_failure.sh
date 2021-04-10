a130a4fe539acaa9aa9b2d25176b009896c1ee0c
CJ Nguyen <cjnguyen.99@gmail.com>
2021-04-10 00:02:09 -0500

    Removed attempt to comment build results

--- scripts/travis_post_failure.sh
+++
@@ -1,5 +0,0 @@
-if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
-    curl -u toastboard${GITHUB_TOKEN} \
-    -d "{\"body\": \"FAILURE check Travis CI logs and revise\"}" \
-    "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
-fi
